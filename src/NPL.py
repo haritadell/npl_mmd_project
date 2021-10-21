ce7de74ee84c19d1b2422f986013281bd7bdc3f0
haritadell <57354452+haritadell@users.noreply.github.com>
2021-10-21 20:04:18 +0100

    Added random restart for toggle switch

--- src/NPL.py
+++ src/NPL.py
@@ -7,16 +7,15 @@ Created on Thu Mar  4 11:28:49 2021
 
 from utils import k, k_jax, k_comp
 import itertools
-from numpy.random import choice
 import numpy as np
 from scipy.stats import dirichlet
 import scipy.spatial.distance as distance
 from tqdm import tqdm
 from joblib import Parallel, delayed
-from scipy import stats
 import ot
 from scipy.optimize import minimize
 import jax.numpy as jnp
+import jax
 from jax import grad, vmap, value_and_grad, jit
 from jax.ops import index_update, index
 from jax.config import config
@@ -45,10 +44,12 @@ class npl():
         self.n, self.d = self.X.shape   
         self.model_name  = model_name 
         self.l = l  
-        # set l = -1 for median heuristic 
         if self.l == -1:
             self.l = np.sqrt((1/2)*np.median(distance.cdist(self.X,self.X,'sqeuclidean')))
-        self.kxx = k(self.X,self.X,self.l)[0] # pre calculate kernel matrix of data k(x,x)
+        if self.model_name == 'toggle_switch':
+            self.kxx = k_comp(self.X,self.X)
+        else:
+            self.kxx = k(self.X,self.X,self.l)[0] # pre calculate kernel matrix of data k(x,x)
         self.s = 1 # standard deviation for Gaussian model
     
         
@@ -60,26 +61,74 @@ class npl():
         # compute weighted log-likelihood minimizer for Gaussian model
         if self.model_name == 'gaussian':
             wll_j = self.WLL(self.X, weights) 
-            was_j = self.minimise_wasserstein(self.X, weights)
         else:
             wll_j = 1 # dummy variable 
-            was_j = 1 # dummy variable
-        
+           
         if self.model_name == 'toggle_switch':
             theta_j = self.minimise_MMD_togswitch(self.X, weights)
         else:
             theta_j = self.minimise_MMD(self.X, weights)
              
-        return theta_j #, wll_j, was_j
+        return theta_j , wll_j 
+    
+    def loss(self, rng, theta):
+        y = self.model.sample(theta)
+        kxy = k_comp(y,self.X)
+        kyy = k_comp(y,y)
+        l = self.MMD_approx(kxy,kyy)
+        return l
+    
+
+    def find_initial_params(self):
+        def sample_theta_init(rng):
+          param_range = (jnp.array([0.01, 0.01, 0.01, 0.01, 250.0, 0.01, 0.01]), jnp.array([50.0, 50.0, 5.0, 5.0, 450.0, 0.5, 0.4]))
+          lower, upper = param_range
+          unparam_theta = jax.random.uniform(rng, minval=lower, maxval=upper, shape=lower.shape)
+          params = jnp.array([jnp.log(unparam_theta[0]),jnp.log(unparam_theta[1]),unparam_theta[2],unparam_theta[3],jnp.log(unparam_theta[4]),jnp.log(unparam_theta[5]),unparam_theta[6]])
+          return params  
+
+        n_initial_locations = 500
+        n_optimized_locations = 3
+
+        rng = jax.random.PRNGKey(2)
+        rng, *rng_inputs = jax.random.split(rng, num=n_initial_locations + 1)
+        init_thetas = vmap(sample_theta_init)(jnp.array(rng_inputs))
+        rng, *rng_inputs = jax.random.split(rng, num=n_initial_locations + 1)
+
+        init_losses = []
+        for t in init_thetas:
+          init_losses.append(self.loss(rng,t))
+
+        rng, *rng_inputs = jax.random.split(rng, num=n_optimized_locations + 1)
+        best_init_params = init_thetas[np.argsort(np.asarray(init_losses))[:n_optimized_locations]]
+
+        return best_init_params 
         
     
     def draw_samples(self):
         """Draws B samples in parallel from the nonparametric posterior"""
         
         # Draw weights for each bootstrap iteration and then parallelize using JAX vmap
-        weights = dirichlet.rvs(np.ones(self.n), size = self.B, random_state = 11)
-        samples = vmap(self.draw_single_sample, in_axes=0)(weights)
+        if self.model_name == 'toggle_switch':
+            self.best_init_params = self.find_initial_params()
+        weights = dirichlet.rvs(np.ones(self.n), size = self.B, random_state = 13)
+        was_samples = np.zeros((self.B,self.p))
+        
+
+        # For the Gaussian model also run NPL with the Wasserstein distance
+        if self.model_name == 'gaussian':
+            temp = Parallel(n_jobs=-1, backend='multiprocessing', max_nbytes=None,batch_size="auto")(delayed(self.minimise_wasserstein)(self.X,weights[i,:]) for i in tqdm(range(self.B)))
+
+            for i in range(self.B):
+                was_samples[i,:] = temp[i]
+                self.was_sample = np.array(was_samples)
+
+        samples, wll_samples = vmap(self.draw_single_sample, in_axes=0)(weights)
         self.sample = np.array(samples) 
+        self.wll_sample = np.array(wll_samples)
+        
+
+
 
     def MMD_approx(self,kxy,kyy):
         """ Approximation of the squared MMD between the model represented by y_{i=1}^m iid sample and
@@ -102,22 +151,25 @@ class npl():
         return (1/(self.m*(self.m-1)))*sum1-(2/(self.n*self.m))*sum2+(1/(self.n*(self.n-1)))*sum3
         
     
-    def minimise_MMD(self, data, weights, Nstep=1000, eta=0.1, batch_size=100):
+    def minimise_MMD(self, data, weights, Nstep=1000, eta=0.1, batch_size=200):
         """Function to minimise the MMD using adam optimisation from jax"""
         
         params=jnp.zeros(self.p)
         if self.model_name == 'gandk':
-            batch_size = 64
-            params = jnp.array([5,5,5,5])
+            batch_size = self.n
+            params = jnp.array([5.,5.,5.,5.])
         
         config.update("jax_enable_x64", True)
         num_batches = self.n//batch_size
         
         # objective function to feed the optimizer
-        def obj_fun(theta, x, n):
-            y = self.model.sample(theta)[0]
-            kyy = k_jax(y,y,self.l)
-            kxy = k_jax(y,x,self.l)
+        def obj_fun(theta, x, n, key):
+            if self.model_name == 'gaussian':
+                y = self.model.sample(theta)
+            else:
+                y = self.model.sample(theta,key)[0]
+            kyy = k_jax(y,y, self.l)
+            kxy = k_jax(y,x, self.l)
 
             # first sum
             diag_elements = jnp.diag_indices_from(kyy)
@@ -134,22 +186,28 @@ class npl():
         opt_state = opt_init(params)
         itercount = itertools.count()
 
-        grad_fn = vmap(jit(grad(obj_fun, argnums=0)), in_axes=(None, 0, None))
+        grad_fn = vmap(jit(grad(obj_fun, argnums=0)), in_axes=(None, 0, None, None))
         
-        def step(step, opt_state, batches):
-            value, grads = grad_fn(get_params(opt_state), batches, batch_size)
+        def step(step, opt_state, batches, key):
+            key, subkey = jax.random.split(key)
+            grads = grad_fn(get_params(opt_state), batches, batch_size, subkey)
             opt_state = opt_update(step, np.mean(grads, axis=0), opt_state) 
-            return value, opt_state
+            return opt_state
         
-        for i in range(Nstep):
+        key1 = jax.random.PRNGKey(11)
+        key2 = jax.random.PRNGKey(13)
+        
+        for i in range(Nstep):            
             batches = []
-            # sample batches
             for _ in range(num_batches):
-                batch_x = choice(data.flatten(), batch_size, p=weights).reshape(-1,1)
-                batches.append(batch_x)
-            batches = np.reshape(batches, (num_batches,batch_size))
+              key1, subkey = jax.random.split(key1)
+              inds = jax.random.choice(subkey, a=self.n, shape=(batch_size,), p=weights) #default is with replacement
+              batch_x = jnp.take(a=data, indices=inds, axis=0)
+              batches.append(batch_x)
+
+            batches = jnp.array(batches)
             # update  
-            value, opt_state = step(next(itercount), opt_state, batches)
+            opt_state = step(next(itercount), opt_state, batches, key2)
             
         return get_params(opt_state)
     
@@ -164,7 +222,6 @@ class npl():
 
         # objective function to feed the optimizer
         def obj_fun(theta, x, n):
-            #y = self.model.sample(theta)[0]
             y = self.model.sample(theta)
             kyy = k_comp(y,y)
             kxy = k_comp(y,x)
@@ -191,7 +248,7 @@ class npl():
             value = np.mean(values, axis=0)
             return value, opt_state
         
-        key = random.PRNGKey(11)
+        key = jax.random.PRNGKey(11)
 
         list_of_thetas = jnp.zeros((n_optimized_locations,7))
         for j in range(n_optimized_locations):
@@ -202,7 +259,6 @@ class npl():
             # get batches 
             batches = []
             for _ in range(num_batches):
-              weights_shape = weights.reshape((self.n,1))
               key, subkey = jax.random.split(key)
               batch_x = jax.random.choice(key, a=data.flatten(), shape=(batch_size,1), p=weights).reshape(-1,1)
               batches.append(batch_x)
@@ -224,8 +280,6 @@ class npl():
             smallest_loss, best_theta = jax.lax.cond(pred, true_func, false_func, [value, smallest_loss, best_theta, opt_state])
 
           list_of_thetas = index_update(list_of_thetas, index[j,:], best_theta)
-
-        loss_min = 10000
         
         losses = []
         seed = 12
@@ -242,9 +296,10 @@ class npl():
     def WLL(self, data, weights):
         """Get weighted log likelihood minimizer, for gaussian model""" 
         
-        theta = 0
+        theta = np.zeros(self.d)
+        print(np.shape(weights))
         for i in range(self.n):
-            theta += weights[i]*data[i] 
+            theta += weights[i]*data[i,:] 
         return theta
     
     def minimise_wasserstein(self, data, weights):
@@ -261,8 +316,8 @@ class npl():
             W = ot.emd2(a, b, M)
             return W
         
-        optimization_result = minimize(wasserstein, np.mean(data,axis=0), method= 'Powell', options={'disp': False, 'maxiter': 10000})
-     
+        optimization_result = minimize(wasserstein, np.zeros(self.p), method= 'Powell', options={'disp': True, 'maxiter': 20000})
+        print(optimization_result.x)
         # return the value at optimum
         return optimization_result.x
     
