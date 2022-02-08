b0cca6b8b2abbeb70ba6744141a9b8f88622cffc
haritadell <57354452+haritadell@users.noreply.github.com>
2022-02-08 15:42:32 +0000

    Added code for extra experiments

--- src/run_gandk.py
+++ src/run_gandk.py
@@ -6,9 +6,10 @@ Created on Wed Sep 29 18:55:21 2021
 @author: HaritaDellaporta
 """
 
-from utils import sample_gandk_outl
-from plot_functions import plot_posterior_marginals, plot_mse, plot_gnk, SeabornFig2Grid
+from utils import sample_gandk_outl, k, MMD_approx
+from plot_functions import plot_gnk, SeabornFig2Grid
 import NPL
+import NPL_prior
 import models
 import numpy as np
 import pandas as pd
@@ -16,6 +17,12 @@ from scipy import stats
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import time
+
+# Before running: 
+# 1) Set paths 
+# 2) Indicate whether you want a fresh dataset or to load existing one 
+# 3) Experiments are run for multiple runs - index which run you want plots for
 
 # Set paths
 data_path = "/Users/HaritaDellaporta/Dropbox/mmd_project_code/data/G_and_k_model/"
@@ -24,6 +31,9 @@ plots_path = "/Users/HaritaDellaporta/Dropbox/mmd_project_code/Plots/G_and_k/"
 
 # Set to True to generate and save fresh datasets or False to load saved datasets
 sample_data_bool = False
+
+# Index which run you want to plot results for in [0,R-1]
+r = 0 
 
 # Set model 
 model_name = 'gandk' 
@@ -78,7 +88,6 @@ np.savetxt(results_path+'NPL_MMD/summary_stats.txt', summary_stats)
 np.savetxt(results_path+'NPL_MMD/cpu_times.txt', times) 
 #%%
 #### Reshaping and plotting results for a single run   
-r = 0 # index which run you want results for
 # Reshape results
 thetas_mmd = np.zeros((p,outl,B))
 for i in range(outl):
@@ -97,7 +106,10 @@ thetas_wabc[3,:,:] = np.log(thetas_wabc[3,:,:])   # last parameter for gandk is 
 # Plot results
 fname1 = plots_path+'run_{}_post_marg.png'.format(r)
 fname2 = plots_path+'bootstrap_it_densities.png'
+fname3 = plots_path+'histograms_sampled_densities.png'
+fname4 = plots_path+'sqrt_n_exp.png'
 
+# Figure 4
 fig = plt.figure(figsize=(13,5))
 gs = gridspec.GridSpec(2, 3)
 axes = plot_gnk(B,thetas_wabc, thetas_mmd, theta_star,outl, fname1,save_fig=False)
@@ -112,15 +124,7 @@ gs.tight_layout(fig)
 #plt.savefig(fname1)
 plt.show()
 
-#%%
-r = 0 # index which run you want results for
-# Reshape results
-thetas_mmd = np.zeros((p,outl,B))
-for i in range(outl):
-    for j in range(p):
-        sample = np.loadtxt(results_path+'NPL_MMD/thetas_mmd_outl_{}_run_{}.txt'.format(i,r))
-        thetas_mmd[j,i,:] = sample[0:500,j]
-        
+# Figure 7 in Appendix        
 outl = 3
 pal = sns.cubehelix_palette(start=.5, rot=-.75, n_colors=B)
 fig, ax_array = plt.subplots(1, outl, figsize=(15,5))
@@ -137,3 +141,290 @@ for j,ax in enumerate(ax_array):
     else:
       ax.set_ylabel('')
 plt.savefig(fname2)
+
+# Figure 6 in Appendix 
+theta_posterior_means = np.mean(thetas_mmd, axis=2)
+theta_posterior_means_wabc = np.mean(thetas_wabc, axis=2)
+f, axes = plt.subplots(2,3,figsize=(15,5))
+for i,ax in enumerate(axes.flatten()):
+  if i <3:
+    X = datasets[0,i,:]
+    samples = sample_gandk_outl(n,d,theta_posterior_means[:,i])
+    ax.hist(X, bins=100, color='#0072B2', alpha = 0.6, label='Observations') 
+    ax.hist(samples, bins=100, color='#D55E00', alpha=0.6, label='Samples')
+    ax.set_title(r'$\epsilon = {}$'.format(i*5/100), fontsize=15)
+    ax.grid(linewidth=0.5)
+    if i == 0:
+      ax.legend(fontsize=15)
+      ax.set_ylabel('NPL-MMD', fontsize=15)
+  else:
+    X = datasets[0,(i-3),:]
+    samples_wabc = sample_gandk_outl(n,d,theta_posterior_means_wabc[:,(i-3)])
+    
+    ax.hist(samples_wabc, bins=100, color='#D55E00', alpha=0.6)
+    ax.hist(X, bins=100, color='#0072B2', alpha = 0.6) 
+    ax.grid(linewidth=0.5)
+    if i > 3:
+      ax.set_xlim(-60,60)
+    else:
+      ax.set_ylabel('WABC', fontsize=15)
+plt.savefig(fname3)
+
+# Figure 8 in Appendix 
+n_range = np.linspace(250,4000,num=10,  dtype=int)
+mmd_dists = np.zeros((len(n_range), R))
+theta_samples = np.zeros((len(n_range),B,p))
+for i, n in enumerate(n_range):
+  print(n)
+  for r in range(R):
+    X = sample_gandk_outl(n,d,theta_star)
+    np.savetxt(data_path+'sample_for_divs_20_{}'.format(n), X)
+    npl_ = NPL.npl(X,B,m,p,l, model = model, model_name = model_name)
+    npl_.draw_samples()
+    theta_est = np.mean(npl_.sample, axis=0)
+    M = 15000
+    x = sample_gandk_outl(M,d,theta_star)
+    y = sample_gandk_outl(M,d,theta_est)
+    kxx = k(x,x,l)[0]
+    kxy = k(y,x,l)[0]
+    kyy = k(y,y,l)[0]
+    mmd_dists[i,r] = MMD_approx(M,M,kxx,kxy,kyy)
+    
+    
+plt.figure(figsize=(5, 3))
+
+plt.plot(n_range, np.sqrt(np.mean(mmd_dists, axis=1)), label='$\sqrt{\mathbb{E}[\widehat{MMD}^2]}$',marker=11, color='#0072B2')
+plt.fill_between(n_range, np.sqrt(np.mean(mmd_dists, axis=1))-np.sqrt(np.std(mmd_dists, axis=1)), np.sqrt(np.mean(mmd_dists, axis=1))+np.sqrt(np.std(mmd_dists, axis=1)), alpha=0.1)
+
+plt.plot(n_range, 2/np.sqrt(n_range), label='$2/\sqrt{n}$', color='#D55E00')
+plt.legend()
+plt.xlabel('n')
+plt.ylabel('$\mathbb{E}[np.sqrt{\widehat{MMD}^2}]$')
+plt.savefig(fname4)
+
+#%%
+# Sensitivity to hyperparameters experiments (Appendix figures 11-14)
+
+# Sensitivity to alpha
+# Range of alphas 
+alphas = np.linspace(0.01,300,num=10)
+
+# Sample or Load data 
+datasets = np.zeros((outl,n))
+for i in range(outl):
+    #X = sample_gandk_outl(n,d,theta_star, n_cont=i).reshape((n,))
+    #np.savetxt(data_path+'alpha_exp_outl_{}'.format(i), X)
+    X = np.loadtxt(data_path+'alpha_exp_outl_{}'.format(i))
+    datasets[i,:] = X
+    
+theta_samples = np.zeros((len(alphas),outl,B,p))
+T = 2**11
+for i, a in enumerate(alphas):
+   print(a)
+   for j in range(outl):
+     X = datasets[j,:].reshape((n,d))
+     npl_ = NPL_prior.npl_prior(X,B,m,s, p,l, model = model, a=a, T=T, model_name = model_name)
+     npl_.draw_samples()
+     sample = npl_.sample
+     theta_samples[i,j,:,:] = sample
+     np.savetxt(results_path+'alpha_exp_theta_outl_{}_alpha_{}_T_{}_2.txt'.format(j,a,T), sample)
+
+# MSE plots  
+mean_theta_samples = np.mean(theta_samples, axis=2)
+def nmse(theta,theta_star):
+    mse_ = np.mean(np.asarray((theta-theta_star))**2)/np.mean(theta_star)
+    return mse_
+
+mses = np.zeros((len(alphas),outl))
+   
+for i in range(len(alphas)):
+  for j in range(outl):
+    mses[i,j] = nmse(mean_theta_samples[i,j,:], theta_star)
+            
+for j in range(outl):
+  plt.plot(alphas, mses[:,j], marker='o', label=r'$\epsilon$ = {}'.format(j*5/100))
+  plt.xlabel(r'$\alpha$', fontsize=20)
+  plt.ylabel('NMSE', fontsize=20)
+  plt.ylim(bottom=0) 
+  plt.ylim(top=0.25)
+  plt.tick_params(axis='both', which='major', labelsize=15)
+  plt.legend(prop={"size":13})
+plt.tight_layout() 
+plt.savefig(results_path+'alpha_mse.png')
+
+# Densities plots
+pal = sns.cubehelix_palette(start=.5, rot=-.75, n_colors=len(alphas))
+fig, ax_array = plt.subplots(1, outl, figsize=(20,6))
+for j,ax in enumerate(ax_array):
+  ax = sns.kdeplot(datasets[0,:].flatten(), ax=ax, linestyle="--", color="black")
+  for i in range(len(alphas)):
+    theta = mean_theta_samples[i,j,:]
+    sample = sample_gandk_outl(n,d,theta, n_cont=0).reshape((n,))
+    ax = sns.kdeplot(sample.flatten(), color=pal[i], ax=ax) #label='n={}'.format(n), #, bw_adjust=1
+    ax.set_title(r'$\epsilon = {}$'.format(j*5/100), fontsize=20)
+    ax.set_xlim(-2,10)
+    ax.set_ylim(0,0.4)
+    ax.set_xlabel('x', fontsize=20)
+    ax.tick_params(axis='both', which='major', labelsize=20)
+    if j == 0:
+      ax.set_ylabel('Density', fontsize=20)
+    else:
+      ax.set_ylabel('')
+
+plt.savefig(results_path+'alpha_densities.png')
+
+# Sensitivity to T 
+# Range of T 
+Ts = [10,100,500,1000,2000,5000]
+
+# Load or sample datasets
+datasets = np.zeros((outl,n))
+for i in range(outl):
+    #X = sample_gandk_outl(n,d,theta_star, n_cont=i).reshape((n,))
+    #np.savetxt(data_path+'alpha_exp_outl_{}'.format(i), X)
+    X = np.loadtxt(data_path+'alpha_exp_outl_{}'.format(i))
+    datasets[i,:] = X
+
+theta_samples = np.zeros((len(Ts),outl,B,p))
+
+a = 0.1  # keep a fixed
+for i, T in enumerate(Ts):
+   print(T)
+   for j in range(outl):
+     X = datasets[j,:].reshape((n,d))
+     npl_ = NPL_prior.npl_prior(X,B,m,s, p,l, model = model, a=a, T=T, model_name = model_name)
+     npl_.draw_samples()
+     sample = npl_.sample
+     theta_samples[i,j,:,:] = sample
+     np.savetxt(results_path+'T_exp_theta_outl_{}_alpha_{}_T_{}_2.txt'.format(j,a,T), sample)
+
+# MSE plot   
+mean_theta_samples = np.mean(theta_samples, axis=2)
+
+for i in range(len(Ts)):
+  for j in range(outl):
+    mses[i,j] = nmse(mean_theta_samples[i,j,:], theta_star)
+    
+for j in range(outl):
+  plt.plot(Ts, mses[:,j], marker='o', label=r'$\epsilon$ = {}'.format(j*5/100))
+  plt.xlabel('T', fontsize=20)
+  plt.ylabel('NMSE', fontsize=20)
+  plt.ylim(bottom=0) 
+  plt.ylim(top=0.035)
+  plt.tick_params(axis='both', which='major', labelsize=15)
+  plt.legend(prop={"size":13})
+plt.tight_layout() 
+plt.savefig(results_path+'a=0.1_2.png')
+
+# Densities plots
+pal = sns.cubehelix_palette(start=.5, rot=-.75, n_colors=len(Ts))
+fig, ax_array = plt.subplots(1, outl, figsize=(20,6))
+for j,ax in enumerate(ax_array):
+  ax = sns.kdeplot(datasets[0,:].flatten(), ax=ax, linestyle="--", color="black")
+  for i in range(len(Ts)):
+    theta = mean_theta_samples[i,j,:]
+    sample = sample_gandk_outl(n,d,theta, n_cont=0).reshape((n,))
+    ax = sns.kdeplot(sample.flatten(), color=pal[i], ax=ax) 
+    ax.set_title(r'$\epsilon = {}$'.format(j*5/100), fontsize=20)
+    ax.set_xlim(-2,10)
+    ax.set_ylim(0,0.4)
+    ax.tick_params(axis='both', which='major', labelsize=20)
+    ax.set_xlabel('x', fontsize=20)
+    if j == 0:
+      ax.set_ylabel('Density', fontsize=20)
+    else:
+      ax.set_ylabel('')
+
+plt.savefig(results_path+'T_exps_a=0.1_2.png')
+
+# Sensitivity to l 
+
+# Range of ls
+ls = np.array([10**(-1), 10**(-0.5), 10**0, 10**(0.5), 10**1, 10**(1.5), 10**2])
+
+datasets = np.zeros((outl,n))
+for i in range(outl):
+    #X = sample_gandk_outl(n,d,theta_star, n_cont=i).reshape((n,))
+    #np.savetxt(data_path+'alpha_exp_outl_{}'.format(i), X)
+    X = np.loadtxt(data_path+'alpha_exp_outl_{}'.format(i))
+    datasets[i,:] = X
+    
+theta_samples = np.zeros((len(ls),outl,B,p))
+
+T = n
+a = 0.01
+for i, l in enumerate(ls):
+   print(l)
+   for j in range(outl):
+     X = datasets[j,:].reshape((n,d))
+     npl_ = npl(X,B,m,s, p,l, model = model, a=a, T=T, model_name = model_name)
+     npl_.draw_samples()
+     sample = npl_.sample
+     theta_samples[i,j,:,:] = sample
+     np.savetxt(results_path+'alpha_exp_theta_outl_{}_l_{}_new.txt'.format(j,l), sample)
+
+mses = np.zeros((len(ls),outl))
+   
+for i in range(len(ls)):
+  for j in range(outl):
+    mses[i,j] = nmse(mean_theta_samples[i,j,:], theta_star)
+
+# Mse plot
+for j in range(outl):
+  plt.plot(ls, mses[:,j], label=r'$\epsilon$ = {}'.format(j*5/100), marker='o')
+  plt.xscale('log', basex=10)
+  plt.xlabel('l', fontsize=20)
+  plt.ylabel('NMSE', fontsize=20)
+  plt.ylim(bottom=0) 
+  plt.ylim(top=40)
+  plt.tick_params(axis='both', which='major', labelsize=15)
+  plt.legend(prop={"size":13})
+plt.tight_layout() 
+plt.savefig(results_path+'l_exp_mses_2_new.png')
+
+# MMD loss plot
+thetas = np.linspace(-40,40,num=200)
+X1 = datasets[0,:].reshape((n,d))
+ls = [0.15,1.,10.]
+kxx = k(X1,X1,l)[0]
+mmds = np.zeros((200,3))
+for j,l in enumerate(ls):
+  for i,theta in enumerate(thetas):
+    thetaa = np.array([3,1,theta,-np.log(2)])
+    y = sample_gandk_outl(n,d,thetaa)
+    kxy = k(y,X1,l)[0]
+    kyy = k(y,y,l)[0]
+    mmd = MMD_approx(n,n,kxx,kxy,kyy)
+    mmds[i,j] = mmd
+    
+plt.plot(thetas,mmds[:,0], label='l=0.15')
+plt.plot(thetas,mmds[:,1], label='l=0.5')
+plt.plot(thetas,mmds[:,2],label='l=1')
+plt.plot(thetas,mmds[:,3], label='l=10')
+plt.xlabel(r'$\theta_3$')
+plt.ylabel(r'$\hat{MMD}$')
+plt.axvline(x=1, color='black')
+plt.legend()
+plt.savefig('losses_theta_3.png')
+
+# Densities plot
+pal = sns.cubehelix_palette(start=.5, rot=-.75, n_colors=len(ls))
+fig, ax_array = plt.subplots(1, outl, figsize=(20,6))
+for j,ax in enumerate(ax_array):
+  ax = sns.kdeplot(datasets[0,:].flatten(), ax=ax, linestyle="--", color="black")
+  for i in range(len(ls)):
+    theta = mean_theta_samples[i,j,:]
+    sample = sample_gandk_outl(n,d,theta, n_cont=0).reshape((n,))
+    ax = sns.kdeplot(sample.flatten(), color=pal[i], ax=ax) 
+    ax.set_title(r'$\epsilon = {}$'.format(j*5/100), fontsize=20)
+    ax.set_xlim(-2,12)
+    ax.set_ylim(0,0.6)
+    ax.tick_params(axis='both', which='major', labelsize=20)
+    ax.set_xlabel('x', fontsize=20)
+    if j == 0:
+      ax.set_ylabel('Density', fontsize=20)
+    else:
+      ax.set_ylabel('')
+
+plt.savefig(results_path+'l_exps_2_new.png')
+
--- src/run_gaussian.py
+++ src/run_gaussian.py
@@ -7,21 +7,31 @@ Created on Wed Sep 29 16:36:07 2021
 """
 
 from utils import sample_gaussian_outl
-from plot_functions import plot_posterior_marginals, plot_mse, SeabornFig2Grid
+from plot_functions import SeabornFig2Grid, plot_gauss_4d_mmd_vs_was, plot_gauss_4d, plot_posterior_marginals_mmd_vs_mabc
 import NPL
 import models
 import numpy as np
 import pandas as pd
 from scipy import stats
 import matplotlib.gridspec as gridspec
+import time
+import matplotlib.pyplot as plt
 
-#
+# Before running: 
+# 1) Set paths 
+# 2) Indicate whether you want a fresh dataset or to load existing one 
+# 3) experiments are run for multiple runs - index which run you want plots for
+
+# Paths
 data_path = "/Users/HaritaDellaporta/Dropbox/mmd_project_code/data/Gaussian_location_model/"
 results_path = "/Users/HaritaDellaporta/Dropbox/mmd_project_code/results/Gaussian_location_model/"
-plots_path = "/Users/HaritaDellaporta/Dropbox/mmd_project_code/Plots/"
+plots_path = "/Users/HaritaDellaporta/Dropbox/mmd_project_code/plots/"
 
 # Set to True to generate and save fresh datasets or False to load saved datasets
 sample_data_bool = False
+
+# Index which run you want to plot results for
+r = 0 
 
 # Set model 
 model_name = 'gaussian' 
@@ -87,29 +97,30 @@ np.savetxt(results_path+'NPL_WLL/summary_stats.txt', summary_stats_wll)
 np.savetxt(results_path+'NPL_MMD/cpu_times.txt', times)     
 #%%
 #### Reshaping and plotting results for a single run   
-r = 0 # index which run you want results for
 # Reshape results
 thetas_mmd = np.zeros((p,outl,B))
 thetas_wll = np.zeros((p,outl,B))
-#thetas_was = np.zeros((p,outl,B))
+thetas_was = np.zeros((p,outl,B))
 for i in range(outl):
     for j in range(p):
         sample = np.loadtxt(results_path+'NPL_MMD/thetas_mmd_outl_{}_run_{}_dim_{}.txt'.format(i,r,d))
         sample_wll = np.loadtxt(results_path+'NPL_WLL/thetas_wll_outl_{}_run_{}_dim_{}.txt'.format(i,r,d))
-        #sample_was = np.loadtxt('/Users/HaritaDellaporta/Dropbox/mmd_project_code/results/outl_{}_{}_{}_{}_was_missp.txt'.format(n_cont,n,d,model_name))
+        sample_was = np.loadtxt(results_path+'NPL_WAS/thetas_mmd_outl_{}_run_{}_dim_{}_.txt'.format(i,r,d))
         if p>1:
             thetas_mmd[j,i,:] = sample[:,j]
             thetas_wll[j,i,:] = sample_wll[:,j]
             
-            #thetas_was[j,n_cont,:] = sample_was[:,j]
+            thetas_was[j,i,:] = sample_was[:,j]
         else:
             thetas_mmd[j,i,:] = sample
             thetas_wll[j,i,:] = sample_wll
-            #thetas_was[j,n_cont,:] = sample_was
+            thetas_was[j,i,:] = sample_was
 thetas_mmd[2,:,:] = np.exp(thetas_mmd[2,:,:])
 thetas_mmd[3,:,:] = np.exp(thetas_mmd[3,:,:])
 thetas_wll[2,:,:] = np.exp(thetas_wll[2,:,:])
 thetas_wll[3,:,:] = np.exp(thetas_wll[3,:,:])
+thetas_was[2,:,:] = np.exp(thetas_was[2,:,:])
+thetas_was[3,:,:] = np.exp(thetas_was[3,:,:])
           
 names = ["X1", "X2", "X3", "X4"]
 thetas_wabc = np.zeros((p,outl,B))
@@ -132,22 +143,30 @@ thetas_mabc[3,:,:] = np.exp(thetas_mabc[3,:,:])
       
 # Plot results
 # Set names for save figures
-fname1 = plots_path+'gauss_dim_{}_run_{}_post_marg'.format(d,r)
-fname2 = plots_path+'gauss_dim_{}_run_{}_mse'.format(d,r) 
-
+fname1 = plots_path+'gauss_dim_{}_run_{}_post_marg_wabc'.format(d,r)
+fname2 = plots_path+'gauss_dim_{}_run{}_post_marg_mabc'.format(d,r)
+#%%
+# Figure 2 in paper
 fig = plt.figure(figsize=(13,5))
-gs = gridspec.GridSpec(2, 3)
-fname1 = plots_path+'run_{}_post_marg_resized'.format(r)
-#axes = plot_gauss_4d(B,thetas_wabc, thetas_mmd, thetas_wll, thetas_wll, theta_star,outl, fname1,save_fig=False)
-axes = plot_gauss_4d_2(B, thetas_mmd, thetas_mabc, theta_star, outl, fname1)
-mg0 = SeabornFig2Grid(axes[0], fig, gs[0], '$\epsilon = 0$', add_title=True)
-mg1 = SeabornFig2Grid(axes[1], fig, gs[1], '$\epsilon = 0.05$', add_title=True)
-mg2 = SeabornFig2Grid(axes[2], fig, gs[2], '$\epsilon = 0.1$', add_title=True)
-mg3 = SeabornFig2Grid(axes[3], fig, gs[3], '0%')
-mg4 = SeabornFig2Grid(axes[4], fig, gs[4], '0%')
-mg5 = SeabornFig2Grid(axes[5], fig, gs[5], '0%')
-
+gs = gridspec.GridSpec(2,3)  
+axes1 = np.take(plot_gauss_4d(B,thetas_wabc, thetas_mmd, thetas_wll, thetas_wll, theta_star,outl, fname1,save_fig=False),[0,2,3,5])
+axes2 = plot_gauss_4d_3(B,thetas_mmd, thetas_was, theta_star, 2, fname1, save_fig=False)
+mg0 = SeabornFig2Grid(axes1[0], fig, gs[0], '$\epsilon = 0$', add_title=True)
+mg1 = SeabornFig2Grid(axes1[1], fig, gs[1], '$\epsilon = 0.1$', add_title=True)
+mg2 = SeabornFig2Grid(axes2[0], fig, gs[2], '$\epsilon = 0.1$', add_title=True)
+mg3 = SeabornFig2Grid(axes1[2], fig, gs[3], '0%')
+mg4 = SeabornFig2Grid(axes1[3], fig, gs[4], '0%')
+mg5 = SeabornFig2Grid(axes2[1], fig, gs[5], '0%')
 gs.tight_layout(fig)
-#plt.savefig(fname1)
+#fig.savefig(fname1)
 plt.show()
 
+# Figure 3 in paper 
+fig = plt.figure(figsize=(8,5))
+gs = gridspec.GridSpec(1,2) 
+axes = plot_posterior_marginals_mmd_vs_mabc(B, thetas_mmd, thetas_mabc, theta_star, 2, fname2)
+mg0 = SeabornFig2Grid(axes[0], fig, gs[0], '$\epsilon = 0.1$')
+mg1 = SeabornFig2Grid(axes[1], fig, gs[1], '$\epsilon = 0.1$')
+gs.tight_layout(fig)
+#fig.savefig(fname2)
+plt.show()
